2dda4a9dcbbe2b83378e82c69f0ca78bc1f963fb
esraa-helmy <helmye02@gmail.com>
2024-01-07 20:06:46 +0200

    add location function to get location

--- main.js
+++ main.js
@@ -155,4 +155,41 @@ locationInputEl.addEventListener('keyup',()=>{
 })
 
 
+// ^ Git Weather Data from your Location
+
+
+let locality;
+function getLocation () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            console.log(position.coords.latitude)
+            console.log(position.coords.longitude)
+            let geoCodingApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`
+    
+            getDataLocation (geoCodingApi)
+            
+        },function(error){
+            console.log(error)
+    
+        })
+        
+    }else{
+        alert("Geolocation does'nt working")
+    
+    }
+    
+}
+async function getDataLocation (geoCodingApi) {
+
+    let hrx = await fetch(geoCodingApi)
+    let response = await hrx.json()
+    console.log(response)
+    console.log(response.locality.toLowerCase())
+    locality= response.locality
+    getData(locality.toLowerCase())
+
+}
+
+ 
+document.getElementById("location").addEventListener('click',getLocation)
 
